279d5fec2a4a6397c7b0a161cbb343a1f7fd1fcd
Adin Ashby <adin.ashby@gmail.com>
2023-09-12 16:35:32 -0400

    Updated

---
+++ MyJavaScriptTests/script.js
@@ -0,0 +1,73 @@
+// var myVariable = 5;
+
+// console.log(myVariable);
+
+// // This is an in-line comment.
+
+// /**
+//  * This is a
+//  * multi-line comment
+//  *
+//  */
+
+// /**
+//  * boolean
+//  * string
+//  * bigint
+//  * number
+//  * object
+//  * symbol
+//  * null
+//  * undefined
+//  */
+
+// myVariable = 8;
+// console.log(myVariable);
+
+// var myString = "Hello";
+// console.log(myString);
+
+// var calculation = 5 + 10;
+// console.log(calculation);
+
+// var myEmptyString = "";
+// var calculation2 = 25 + myEmptyString;
+
+// console.log(calculation2);
+
+// console.log(typeof calculation);
+// console.log(typeof calculation2);
+
+// const THE_COLLEGE = "Vanier College";
+// console.log(THE_COLLEGE);
+// // THE_COLLEGE = "Dawson College"; ERROR
+
+// var myNumber = 100;
+
+// myNumber++;
+// console.log(myNumber);
+
+// myNumber--;
+// console.log(myNumber);
+
+let myCalc = 2.1 + 6.1;
+console.log(myCalc);
+
+//console.log(Math.floor(myCalc));
+//console.log(Math.round(myCalc));
+console.log(Math.ceil(myCalc));
+
+myCalc += 4.3;
+// myCalc = myCalc + 4.3
+console.log(myCalc);
+
+let myConcat = "This is " + 'a tutorial "about JavaScript"';
+myConcat += " at Vanier College";
+console.log(myConcat);
+
+console.log(myConcat.toLowerCase());
+console.log(myConcat.toUpperCase());
+console.log("\n");
+console.log(myConcat);
+
+console.log(myConcat.length);
---
+++ MyJavaScriptTests/script3.js
@@ -0,0 +1,61 @@
+let myName = "Adin Ashby";
+//const firstLetter = myName.charAt(0);
+// const firstLetter = myName[1];
+
+// console.log("first letter is: " + firstLetter);
+// console.log(myName.length);
+
+// console.log(myName[myName.length - 1]);
+
+// // myName[0] = "B";
+// myName = "James";
+// console.log(myName);
+
+// const colleges = ["Vanier", "Dawson", "John Abbott"];
+
+// console.log(colleges);
+// console.log(colleges[1]);
+
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// const grades = [
+//   ["George", 75],
+//   ["Maria", 95],
+// ];
+
+// console.log(grades);
+
+// console.log(grades[0][0]);
+// console.log(grades[1][1]);
+
+// console.log(grades[1][2]);
+
+// colleges.push("Vieux Montreal");
+
+// console.log(colleges);
+
+// let myTest = 4;
+// console.log(myTest);
+
+// myTest = "Hello";
+// console.log(myTest);
+// console.log(typeof myTest);
+
+// colleges.pop();
+// console.log(colleges);
+
+// colleges.shift();
+// console.log(colleges);
+
+// colleges.unshift("Vanier College");
+// console.log(colleges);
+
+function addNumbers(num1, num2) {
+  let myAccessTest = 999;
+  return num1 + num2;
+}
+
+// console.log(myAccessTest); ERROR
+
+console.log(addNumbers(5, 7));
